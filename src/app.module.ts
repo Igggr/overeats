0d1e5994b1a59276c35386e49bdbe22e9bee9efe
Igor Smirnov <uchetzapis@yahoo.com>
2022-12-15 14:33:52 +0300

    Use env file for DB setttings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantResolver } from './restaurant/restaurant-resolver';
@@ -8,17 +9,22 @@ import { RestaurantModule } from './restaurant/restaurant.module';
 @Module({
   imports: [
     RestaurantModule,
+    ConfigModule.forRoot({
+      envFilePath: `env/.env.${process.env.NODE_ENV}`,
+      ignoreEnvFile: process.env.NODE_ENV ==='prod',
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true, // генерировать на лету
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: '123456',
-      database: 'test',
+      host: process.env.LOCALHOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [],
       synchronize: true,
     }),
